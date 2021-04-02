d0df2c1e4513920ff366688c0b6f925a31981f9e
Silverio Manuel Rosales Santana <silverio_m@hotmail.com>
2021-04-02 02:48:46 +0200

    Inicio del proyecto.

---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+/**
+ * 
+ */
+/**
+ * @author Silverio Manuel Rosales Santana
+ * @date 02042021
+ * @version 1.0
+ *
+ */
+module pfgPandemy {
+}
---
+++ src/vista/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Silverio Manuel Rosales Santana
+ * @date 02042021
+ * @version 1.0
+ *
+ */
+package vista;
